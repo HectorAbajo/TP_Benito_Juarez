68de2b44e8f9debc8e4ed1ec1924a9fd8f16921d
HectorAbajo <110790345+HectorAbajo@users.noreply.github.com>
2023-06-27 18:03:14 -0300

    Add files via upload

---
+++ TP1.RegAotomotor/Auto.ts
@@ -0,0 +1,19 @@
+export class Auto {
+    
+    marca : string;
+    tipo : string;
+    anio : string;
+    combustible : string;
+    nMotor : string;
+    nChasis : string;
+
+    constructor(marca : string , tipo : string , anio : string , combustible : string ,nMotor : string , nChasis : string){
+        this.marca = marca
+        this.tipo = tipo
+        this.anio = anio
+        this.combustible = combustible
+        this.nMotor = nMotor
+        this.nChasis = nChasis
+    }
+
+}
